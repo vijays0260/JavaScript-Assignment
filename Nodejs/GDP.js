42cb9a1dade62717f039dea0cc3bd0dd99b0a5c7
vijays0260 <vijays0260@yahoo.com>
2016-08-09 18:26:59 +0530

    final added

--- Nodejs/GDP.js
+++ Nodejs/GDP.js
@@ -3,34 +3,87 @@ var filedata = fs.readFileSync("Table_1.3.csv");
  var datatoString=filedata.toString();
  var myArray= datatoString.split('\r\n');
  var header=myArray[0].split(',');
- var row=myArray.length;
+
+  var row=myArray.length;
  var col=header.length;
- var jArray=[];
- var Cont_Array=[];
- var population = header.indexOf("Population (Millions) 2013");
+  var newArray=[];
+ var arrayValue=[];
+
+var population = header.indexOf("Population (Millions) 2013");
  var gdp = header.indexOf("GDP Billions (USD) 2013");
  var ppp = header.indexOf("Gross domestic product based on Purchasing-Power-Parity (PPP) valuation of Country GDP in Billions (Current International Dollar) 2013");
- csvtojson(population,"country_pop.json");
- csvtojson(gdp,"country_gdp.json");
- csvtojson(ppp,"country_ppp.json");
 
- function csvtojson(index,filename){
- jArray=[];
- var i=0,j=0;
- for (i = 1; i < row-1; i++) {
-    var obj = {};
-    var myNewLine=myArray[i].split(',');
-    for (j = 0; j< col; j++) {
-      key = header[0];
-      value = myNewLine[0];
-      key1 = header[index];
-      value1 = myNewLine[index];
-      obj[key] = value;
-      obj[key1] = value1;
-    }
-      jArray.push(obj);
+ var continents = [
+     ["Asia","Africa","Europe","NorthAmerica","SouthAmerica","Australia"],
+     ["China","India","Indonesia","Japan","Republic of Korea","Saudi Arabia"],
+     ["South Africa"],
+     ["France","Germany","Italy","Russia","Turkey","United Kingdom","European Union"],
+     ["Canada","Mexico","USA"],
+     ["Argentina","Brazil"],
+    ["Australia"]];
+
+ population1 = [0,0,0,0,0,0];
+ gdp1 = [0,0,0,0,0,0];
+
+ for (var j = 1; j < continents.length; j++) {
+for (var k = 0; k < continents[j].length; k++) {
+ for (var i = 1; i < row-1; i++) {
+   var myNewLine=myArray[i].split(',');
+   if(myNewLine[0] === continents[j][k])
+   {
+       population1[j-1] +=  parseFloat(myNewLine[population]);
+       gdp1[j-1] += parseFloat(myNewLine[gdp]);
+   }
+ }
+}
+}
+
+
+var obj1 = {};
+for (var i = 0; i < continents[0].length; i++)
+ {
+   continent_key = 'Continent';
+ continent_value = continents[0][i];
+  population_key = 'Population';
+ population_value = population1[i];
+ gdp_key = 'GDP';
+  gdp_value = gdp1[i];
+ obj1[continent_key] = continent_value;
+ obj1[population_key] = population_value;
+ obj1[gdp_key] = gdp_value;
+ newArray.push(obj1);
+var obj1 = {};
 
 }
-console.log( jArray);
+ console.log(newArray);
+/*
+ fs.writeFile( "aggregate.json", JSON.stringify( Cont_Array ), "utf8", function(err) {
+ if (err) throw err;
+ });
+*/
+ csvtojson(population,"Population.json");
+ csvtojson(gdp,"GDP.json");
+ csvtojson(ppp,"PPP.json");
+
+ function csvtojson(index,filename){
+ newArray=[];
+ var i=0,j=0;
+ for (i = 1; i < row-2; i++) {
+    var newObj = {};
+    var newLine=myArray[i].split(',');
+
+    key = header[0];
+      country = newLine[0];
+    key1 = header[index];
+      value1 = newLine[index];
+     newObj[key] = country;
+      newObj[key1] = value1;
+      newArray.push(newObj);
+
+}
+   console.log( newArray);
+    fs.writeFile(filename, JSON.stringify( arrayValue ), "utf8", function(err) {
+    if (err) throw err;
+});
 
 }
